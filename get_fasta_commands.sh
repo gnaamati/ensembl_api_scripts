df76067cd8af16eeed7084fc7d40764b428af691
Guy Naamati <gnaamati@ebi.ac.uk>
2019-02-25 16:01:31 +0000

    get fasta commands

---
+++ get_fasta_commands.sh
@@ -0,0 +1,80 @@
+echo getting data for Bushbaby
+perl get_genes.pl Bushbaby > Bushbaby.genes.fa
+perl get_aa.pl Bushbaby > Bushbaby.aa.fa
+
+echo getting data for Chimpanzee
+perl get_genes.pl Chimpanzee > Chimpanzee.genes.fa
+perl get_aa.pl Chimpanzee > Chimpanzee.aa.fa
+
+echo getting data for Gibbon
+perl get_genes.pl Gibbon > Gibbon.genes.fa
+perl get_aa.pl Gibbon > Gibbon.aa.fa
+
+echo getting data for Gorilla
+perl get_genes.pl Gorilla > Gorilla.genes.fa
+perl get_aa.pl Gorilla > Gorilla.aa.fa
+
+echo getting data for Human
+perl get_genes.pl Human > Human.genes.fa
+perl get_aa.pl Human > Human.aa.fa
+
+echo getting data for Macaque
+perl get_genes.pl Macaque > Macaque.genes.fa
+perl get_aa.pl Macaque > Macaque.aa.fa
+
+echo getting data for Marmoset
+perl get_genes.pl Marmoset > Marmoset.genes.fa
+perl get_aa.pl Marmoset > Marmoset.aa.fa
+
+echo getting data for Mouse_Lemur
+perl get_genes.pl Mouse_Lemur > Mouse_Lemur.genes.fa
+perl get_aa.pl Mouse_Lemur > Mouse_Lemur.aa.fa
+
+echo getting data for Olive_baboon
+perl get_genes.pl Olive_baboon > Olive_baboon.genes.fa
+perl get_aa.pl Olive_baboon > Olive_baboon.aa.fa
+
+echo getting data for Orangutan
+perl get_genes.pl Orangutan > Orangutan.genes.fa
+perl get_aa.pl Orangutan > Orangutan.aa.fa
+
+echo getting data for Tarsier
+perl get_genes.pl Tarsier > Tarsier.genes.fa
+perl get_aa.pl Tarsier > Tarsier.aa.fa
+
+echo getting data for Vervet-AGM
+perl get_genes.pl Vervet-AGM > Vervet-AGM.genes.fa
+perl get_aa.pl Vervet-AGM > Vervet-AGM.aa.fa
+
+echo getting data for Guinea_Pig
+perl get_genes.pl Guinea_Pig > Guinea_Pig.genes.fa
+perl get_aa.pl Guinea_Pig > Guinea_Pig.aa.fa
+
+echo getting data for Kangaroo_rat
+perl get_genes.pl Kangaroo_rat > Kangaroo_rat.genes.fa
+perl get_aa.pl Kangaroo_rat > Kangaroo_rat.aa.fa
+
+echo getting data for Mouse
+perl get_genes.pl Mouse > Mouse.genes.fa
+perl get_aa.pl Mouse > Mouse.aa.fa
+
+echo getting data for Pika
+perl get_genes.pl Pika > Pika.genes.fa
+perl get_aa.pl Pika > Pika.aa.fa
+
+echo getting data for Rabbit
+perl get_genes.pl Rabbit > Rabbit.genes.fa
+perl get_aa.pl Rabbit > Rabbit.aa.fa
+
+echo getting data for Rat
+perl get_genes.pl Rat > Rat.genes.fa
+perl get_aa.pl Rat > Rat.aa.fa
+
+echo getting data for Squirrel
+perl get_genes.pl Squirrel > Squirrel.genes.fa
+perl get_aa.pl Squirrel > Squirrel.aa.fa
+
+echo getting data for Tree_Shrew
+perl get_genes.pl Tree_Shrew > Tree_Shrew.genes.fa
+perl get_aa.pl Tree_Shrew > Tree_Shrew.aa.fa
+
